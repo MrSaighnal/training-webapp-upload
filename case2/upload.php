cead7c6f92cbf8e3907bb30a8e01965e006ecbe6
MrSaighnal <47419260+MrSaighnal@users.noreply.github.com>
2023-03-02 15:12:22 +0100

    Update upload.php

--- case2/upload.php
+++ case2/upload.php
@@ -5,16 +5,6 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
 
 // Check if file already exists
 if (file_exists($target_file)) {
@@ -24,7 +14,7 @@ if (file_exists($target_file)) {
 
 
 // Allow certain file formats
-if($imageFileType == "php" ) {
+if($imageFileType == "php" || $imageFileType == "htaccess") {
   echo "Sorry, extension not allowed.";
   $uploadOk = 0;
 }
